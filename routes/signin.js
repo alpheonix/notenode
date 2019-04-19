dc7bf1571992a90b04901cc3fe06fdeb0d61b16b
Arthur BLANC <ablanc@vente-privee.com>
2019-04-19 13:53:16 +0200

    error code

--- routes/signin.js
+++ routes/signin.js
@@ -1,59 +1,72 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 
-const User = require('../model/user');
-const config = require('../config/config');
-
+const User = require("../model/user");
+const config = require("../config/config");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.send('connction');
+router.get("/", function(req, res, next) {
+  res.send("connction");
 });
-
 
 /* Post  home page. */
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(400).json({
+      error: "Le champ utilisateur ou password est vide",
+      token: undifined
+    });
+  }
 
-    if (!req.body.username || !req.body.password) {
-    res.status(400).send("Le champ utilisateur ou password est vide");
-    }
+  const hasLowerCase = str => /\b([a-z]+)\b/.test(str);
 
-    const hasLowerCase = (str) => (/\b([a-z]+)\b/.test(str));
+  if (!hasLowerCase(req.body.username)) {
+    res.status(400).json({
+      error:
+        "Votre identifiant ne doit contenir que des lettres minuscules non accentuées",
+      token: undifined
+    });
+  }
 
-    if(!hasLowerCase(req.body.username)){
-        res.status(400).send("Votre identifiant ne doit contenir que des lettres minuscules non accentuées")
-    }
+  if (req.body.username.length < 2 || req.body.username.length > 20) {
+    res.status(400).json({
+      error: "Votre identifiant doit contenir entre 2 et 20 caractères",
+      token: undifined
+    });
+  }
 
-    if(req.body.username.length < 2 || req.body.username.length > 20){
-        res.status(400).send("Votre identifiant doit contenir entre 2 et 20 caractères")
-    }
-
-    User.findOne({
-        username: req.body.username
-    }, (err, user) => {
-        if (err) {
-            res.status(500).send("Erreur interne");
-        }
-        if(!user){
-            res.status(401).send("Cet identifiant est inconnu");
-        }
-        if (user.authenticate(req.body.password, user.password)) {
-            jwt.sign({ userID: user._id }, config.secret, { expiresIn:'20min' }, (err,token) => {
-                res.status(200).json({
-                    "token": token,
-                    "text": "Authentification réussi"
-                });
+  User.findOne(
+    {
+      username: req.body.username
+    },
+    (err, user) => {
+      if (err) {
+        res.status(500).send("Erreur interne");
+      }
+      if (!user) {
+        res.status(403).send({
+          error: "Cet identifiant est inconnu",
+          token: undifined
+        });
+      }
+      if (user.authenticate(req.body.password, user.password)) {
+        jwt.sign(
+          { userID: user._id },
+          config.secret,
+          { expiresIn: "20min" },
+          (err, token) => {
+            res.status(200).json({
+              error: null,
+              token: token
             });
-        } else {
-            res.status(401).send("Le mot de passe est incorrect");
-        }
-    })
-
+          }
+        );
+      } else {
+        res.status(403).json("Le mot de passe est incorrect");
+      }
+    }
+  );
 });
-
-
-
-
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -1,80 +1,101 @@
-const  express = require('express');
-const  router = express.Router();
-const User = require('../model/user');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
+const User = require("../model/user");
 const passwordHash = require("password-hash");
 
-
-
-
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-    res.send('inscription');
+router.get("/", (req, res, next) => {
+  res.send("inscription");
 });
 
-
 /* POST users listing. */
-router.post('/', (req, res, next) => {
-    if (!req.body.username || !req.body.password) {
-        res.status(400).send("Le champ utilisateur ou password est vide");
-    }
-
-    const hasLowerCase = (str) => (/\b([a-z]+)\b/.test(str));
-
-    if(!hasLowerCase(req.body.username)){
-      res.status(400).send("Votre identifiant ne doit contenir que des lettres minuscules non accentuées")
-    }
-
-    if(req.body.username.length < 2 || req.body.username.length > 20){
-      res.status(400).send("Votre identifiant doit contenir entre 2 et 20 caractères")
-    }
-
-    const  user = {
-        username: req.body.username,
-        password: passwordHash.generate(req.body.password)
-    }
-
-    // On vérifie que l'utilisateur n'existe pas dans la base de données
-
-    const  findUser = new Promise((resolve, reject) => {
-        User.findOne({
-            username: user.username
-        }, (err, result) => {
-            if (err) {
-                reject(500);
-            }
-            if (result) {
-                reject(204)
-            } else {
-                resolve(true)
-            }
-        })
+router.post("/", (req, res, next) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(400).json({
+      error: "Le champ utilisateur ou password est vide",
+      token: undifined
     });
+  }
 
-    findUser.then(() => {
-        const newUser = new User(user);
+  const hasLowerCase = str => /\b([a-z]+)\b/.test(str);
+
+  if (!hasLowerCase(req.body.username)) {
+    res.status(400).json({
+      error:
+        "Votre identifiant ne doit contenir que des lettres minuscules non accentuées",
+      token: undifined
+    });
+  }
+
+  if (req.body.username.length < 2 || req.body.username.length > 20) {
+    res.status(400).json({
+      error: "Votre identifiant doit contenir entre 2 et 20 caractères",
+      token: undifined
+    });
+  }
+
+  const user = {
+    username: req.body.username,
+    password: passwordHash.generate(req.body.password)
+  };
+
+  // On vérifie que l'utilisateur n'existe pas dans la base de données
+
+  const findUser = new Promise((resolve, reject) => {
+    User.findOne(
+      {
+        username: user.username
+      },
+      (err, result) => {
+        if (err) {
+          reject(500);
+        }
+        if (result) {
+          reject(204);
+        } else {
+          resolve(true);
+        }
+      }
+    );
+  });
+
+  findUser.then(
+    () => {
+      const newUser = new User(user);
       newUser.save((err, user) => {
         if (err) {
-            res.status(500).send("Erreur interne")
+          res.status(500).send("Erreur interne");
         }
-        res.status(200).json({
-            "text": "Succès ^^",
-            "token": user.getToken()
-        });
+        jwt.sign(
+          { userID: user._id },
+          config.secret,
+          { expiresIn: "20min" },
+          (err, token) => {
+            res.status(200).json({
+              error: null,
+              token: token
+            });
+          }
+        );
       });
-    }, (error) => {
-        switch (error) {
-            case 500:
-                res.status(500).send("Erreur interne")
-                break;
-            case 204:
-              res.status(400).send("Cet identifiant est déjà associé à un compte");
-                break;
-            default:
-                res.status(500).send("Erreur interne")
-        }
-    })
-
-
+    },
+    error => {
+      switch (error) {
+        case 500:
+          res.status(500).send("Erreur interne");
+          break;
+        case 204:
+          res.status(400).json({
+            error: "Cet identifiant est déjà associé à un compte",
+            token: undifined
+          });
+          break;
+        default:
+          res.status(500).send("Erreur interne");
+      }
+    }
+  );
 });
 
 module.exports = router;
