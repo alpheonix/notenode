36f0b872ab55f446ae98eccbfea9bf4665592473
Tristan Luong <tristan.luong@gmail.com>
2019-04-19 14:35:10 +0200

    Refacto how we update and delete using session token

--- routes/notes.js
+++ routes/notes.js
@@ -7,12 +7,15 @@ const Note = require('../models/notes');
 
 router.put('/', verifyToken, (req, res) => {
     jwt.verify(req.token, config.secret, (err, authData) => {
-
+        if(err) {
+          res.status(403).json({
+            error: "Veuillez vous reconnecter"
+          });
+        }
         const note = Note({
             userId: authData.userId,
             content: req.body.content,
         });
-
         note.save((result) => {
             res.send(result);
         });
@@ -27,8 +30,7 @@ router.get('/', verifyToken, (req, res) => {
             });
         }
 
-        Note.find({ userId: authData.userId }, (err, notes) => {
-            console.log(notes)
+        Note.find({ userId: authData.userId }, (err, note) => {
             if(!notes.length){
                 res.status(400).json({
                   error: "Cet utilisateur n'a pas de notes",
@@ -36,89 +38,53 @@ router.get('/', verifyToken, (req, res) => {
             } else {
               res.status(200).json({
                 error: err,
-                notes: notes
+                note: note
               })
             }
         });
     });
 });
 
-router.patch('/:id', verifyToken,function(req, res, next) {
-    jwt.verify(req.token,config.secret,(err,authData)=>{
-        if(err){
-            res.status(403);
-        }else{
-            if (!req.body.note || !req.param.id) {
-                //Le cas où l'email ou bien le password ne serait pas soumit ou nul
-                res.status(400).json({
-                    "text": "Requête NON  valide"
-                })
-            } else {
-                (async function (resolve, reject) {
-                    await Note.update({ userID: req.param.id }, { content: req.body.note,lastUpdateAt: new Date() }, options, callback)
-          
-                }).then(function (error) {
-                    switch (error) {
-                        case 500:
-                            res.status(500).json({
-                                "text": "Erreur interne"
-                            })
-                            break;
-                        case 204:
-                            res.status(204).json({
-                                "text": "L'adresse email existe déjà"
-                            })
-                            break;
-                        default:
-                            res.status(500).json({
-                                "text": "Erreur interne"
-                            })
-                    }
-                })
-          
-            }
-        }
+router.patch('/:id', verifyToken, (req, res) => {
+  jwt.verify(req.token, config.secret, (err, authData) => {
+    if (err) {
+      res.status(403).json({
+        error: "Veuillez vous reconnecter"
+      });
+    }
+    Note.update({_id: req.params.id}, {content: req.body.content, lastUpdateAt: new Date()}, (err, note) => {
+      if (err) {
+        res.status(400).json({
+          error: "Erreur lors de la mise à jour de votre note",
+          note: note
         });
-    
-  
-
+      }
+      else {
+        res.status(200).json({
+          error: err,
+          note: note
+        });
+      }
+    });
+  });
 });
 
-router.delete('/:id', verifyToken,function(req, res, next) {
-    jwt.verify(req.token,config.secret,(err,authData)=>{
-        if(err){
-            res.status(403);
-        }else{
-            if (!req.param.id) {
-                //Le cas où l'email ou bien le password ne serait pas soumit ou nul
-                res.status(400).json({
-                    "text": "Requête NON  valide"
-                })
-            } else {
-                (async function (resolve, reject) {
-                    await Note.deleteOne( { _id : ObjectId(req.param.id) } );
-          
-                }).then(function (error) {
-                    switch (error) {
-                        case 500:
-                            res.status(500).json({
-                                "text": "Erreur interne"
-                            })
-                            break;
-                        case 204:
-                            res.status(204).json({
-                                "text": "L'adresse email existe déjà"
-                            })
-                            break;
-                        default:
-                            res.status(500).json({
-                                "text": "Erreur interne"
-                            })
-                    }
-                })
-            }
-        }
+router.delete('/:id', verifyToken, (req, res) => {
+  jwt.verify(req.token, config.secret , (err, authData) => {
+      Note.deleteOne( { _id : req.params.id }, (err, note) => {
+      if(err){
+        res.status(400).json({
+          error: "Erreur lors de la suppression de votre note",
+          note: note
+        })
+      } else {
+        res.status(200).json({
+          error: err,
+          note: note
+        })
+      }
     });
+  });
 });
 
 function verifyToken(req,res,next){
--- routes/signin.js
+++ routes/signin.js
@@ -5,52 +5,6 @@ const router = express.Router();
 const User = require("../model/user");
 const config = require("../config/config");
 
-/* GET home page. */
-router.get("/", function(req, res, next) {
-  res.send("connction");
-});
-
-/* Post  home page. */
-<<<<<<< HEAD
-router.post('/', (req, res) => {
-
-    if (!req.body.username || !req.body.password) {
-    res.status(400).send("Le champ utilisateur ou password est vide");
-    }
-
-    const hasLowerCase = (str) => (/\b([a-z]+)\b/.test(str));
-
-    if(!hasLowerCase(req.body.username)){
-        res.status(400).send("Votre identifiant ne doit contenir que des lettres minuscules non accentuées")
-    }
-
-    if(req.body.username.length < 2 || req.body.username.length > 20){
-        res.status(400).send("Votre identifiant doit contenir entre 2 et 20 caractères")
-    }
-
-    User.findOne({
-        username: req.body.username
-    }, (err, user) => {
-        if (err) {
-            res.status(500).send("Erreur interne");
-        }
-        if(!user){
-            res.status(401).send("Cet identifiant est inconnu");
-        } else {
-          if (user.authenticate(req.body.password, user.password)) {
-            jwt.sign({userId: user._id}, config.secret, {expiresIn: '20min'}, (err, token) => {
-              res.status(200).json({
-                "token": token,
-                "text": "Authentification réussi"
-              });
-            });
-          } else {
-            res.status(401).send("Le mot de passe est incorrect");
-          }
-        }
-    })
-
-=======
 router.post("/", (req, res) => {
   if (!req.body.username || !req.body.password) {
     res.status(400).json({
@@ -107,7 +61,6 @@ router.post("/", (req, res) => {
       }
     }
   );
->>>>>>> dc7bf1571992a90b04901cc3fe06fdeb0d61b16b
 });
 
 module.exports = router;
